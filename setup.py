d3890155f08c3635699b8b1238275b63fc9ae149
Ermo Wei <nosyndicate@gmail.com>
2017-11-08 14:55:21 -0500

    add findpackage

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 setup(name='gym_soccer',
       version='0.0.1',
       install_requires=['gym>=0.2.3',
-                        'hfo_py>=0.2']
+                        'hfo_py>=0.2'],
+      packages=[package for package in find_packages()
+                if package.startswith('gym')],
 )
