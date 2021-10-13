63553d4c9d7c7bdc4ec678a357a32c0a8b6bd3cd
李可 <like2@CN0014008466M.local>
2021-10-13 18:40:45 +0800

    add_install_packages

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(name='gym_soccer',
       version='0.0.1',
+      packages=['gym_soccer'],
       install_requires=['gym>=0.2.3',
                         'hfo_py>=0.2']
 )
