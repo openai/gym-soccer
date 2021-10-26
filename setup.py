82912c311dd665bbacb3d804fdb60ddf17962659
niuyazhe <niuyazhe@sensetime.com>
2021-10-26 21:43:27 +0800

    fix(nyz): fix install recursive directory bug

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_soccer',
       version='0.0.1',
-      packages=['gym_soccer'],
+      packages=[*find_packages(include=('gym_soccer', "gym_soccer.*")),],
       install_requires=['gym>=0.2.3',
                         'hfo_py>=0.2']
 )
